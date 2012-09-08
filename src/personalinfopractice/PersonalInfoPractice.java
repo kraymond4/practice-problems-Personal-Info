6fa72fc0cac2cbd809a0518efb16775abb3145fb
Owner <Owner@Owner-PC>
2012-09-08 09:41:58 -0500

    practice problem
    "Starting out with Java-early objects"
    chapter 3, Page 178, Problem 3
    
    Start

---
+++ src/personalinfopractice/PersonalInfoPractice.java
@@ -0,0 +1,24 @@
+/*
+ * "Starting out with java"
+ * chapter 3  Page 178
+ * Problem 3
+ * 
+ * for practice
+ */
+package personalinfopractice;
+
+/**
+ *
+ * @author Kyle Raymond
+ */
+public class PersonalInfoPractice 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+}
