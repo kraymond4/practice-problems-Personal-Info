49ca95308293da5f64867c37cd9d9fee391340d6
Owner <Owner@Owner-PC>
2012-09-08 09:58:21 -0500

    practice problem
    "Starting out with Java-early objects"
    chapter 3, Page 178, Problem 3
    
    Basic class done

--- src/personalinfopractice/PersonInfo.java
+++ src/personalinfopractice/PersonInfo.java
@@ -10,5 +10,60 @@ package personalinfopractice;
  */
 public class PersonInfo 
 {
+    private String name;
+    private String address;
+    private int age;
+    private String phoneNum;
     
+    public PersonInfo ()
+    {
+        name = "";
+        address = "";
+        age = 0;
+        phoneNum = "000-000-0000";
+        
+    }
+    
+    public void SetName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String GetName()
+    {
+        return name;
+    }
+    
+    
+    public void SetAddress(String address)
+    {
+        this.address = address;
+    }
+    
+    public String GetAddress()
+    {
+        return address;
+    }
+    
+    
+    public void SetAge(int age)
+    {
+        this.age = age;
+    }
+    
+    public int GetAge()
+    {
+        return age;
+    }
+    
+    
+    public void SetPhoneNumber(String phone)
+    {
+        this.phoneNum = phone;
+    }
+    
+    public String GetPhoneNumber()
+    {
+        return phoneNum;
+    }
 }
