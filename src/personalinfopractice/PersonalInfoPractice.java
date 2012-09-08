8b61ff6a96b1b2e878c2ec943c1b14854de11d8b
Owner <Owner@Owner-PC>
2012-09-08 12:29:07 -0500

    practice problem
    "Starting out with Java-early objects"
    chapter 3, Page 178, Problem 3
    
    fine tuning part two
    
    doing stuff not suggested in the book problem

--- src/personalinfopractice/PersonalInfoPractice.java
+++ src/personalinfopractice/PersonalInfoPractice.java
@@ -21,14 +21,22 @@ public class PersonalInfoPractice
     {
         PersonInfo dude = new PersonInfo();
         
-        dude.SetName("Johnny");
+        dude.SetName("Johnny S. Toner");
         dude.SetAddress("404 Whatever Street");
         dude.SetAge(26);
         dude.SetPhoneNumber("343-555-6969");
         
-        PersonInfo oldFart = new PersonInfo("Mister Crusty", 
+        PersonInfo oldFart = new PersonInfo("Mr. Crabby B. Ottum", 
                 "105 Gitoffmylawn Avenue", 75, "111-222-1234");
         
+        dude.SetCatchPhrase("Whoa, Dude.");
+        
+        oldFart.SetCatchPhrase("Why in my day...");
+        
+        System.out.println("Let me tell you a story.");
+        System.out.println("There was once an elderly man named " + oldFart.GetName() +".");
+        System.out.println("He lived on " + oldFart.GetAddress() + ", and was busy trying to solve a crossword puzzle.");
+        System.out.println("Meanwhile, in the distant land called " + dude.GetAddress() + ", a crazy, impossible plot was being hatched to order ");
         
     }
 }
